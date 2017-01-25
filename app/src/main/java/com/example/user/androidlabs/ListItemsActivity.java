83ed6cd5b60eca3205cb6c4cd5a7e8fa7c6c80a0
jschmuland <joelschmuland@gmail.com>
2017-01-25 14:32:15 -0500

    Working on Lab 3

--- app/src/main/java/com/example/user/androidlabs/ListItemsActivity.java
+++ app/src/main/java/com/example/user/androidlabs/ListItemsActivity.java
@@ -1,13 +1,46 @@
 package com.example.user.androidlabs;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ListItemsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("ListItemsActivity", "In onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i("ListItemsActivity", "In onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i("ListItemsActivity", "In onStart()");
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i("ListItemsActivity", "In onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i("ListItemsActivity", "In onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i("ListItemsActivity", "In onDestroy()");
+        super.onDestroy();
     }
 }
