743ae05316349df083ba184ef3eb99ad5a0f02da
jschmuland <joelschmuland@gmail.com>
2017-01-31 17:04:51 -0500

    Finished Lab 3

--- app/src/main/java/com/example/user/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/user/androidlabs/LoginActivity.java
@@ -1,16 +1,36 @@
 package com.example.user.androidlabs;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("LoginActivity", "In onStart");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -24,6 +44,90 @@ public class LoginActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        EditText startEmail = (EditText) findViewById(R.id.loginID);
+        SharedPreferences pref = getSharedPreferences("login",Context.MODE_PRIVATE);
+        startEmail.setText(pref.getString("emailTemp", "email@domain.com"));
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+
+    @Override
+    protected void onResume() {
+        Log.i("LoginActivity", "In onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i("LoginActivity", "In onStart()");
+        super.onStart();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        final SharedPreferences prefs = this.getSharedPreferences("login", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor edit = prefs.edit();
+        final EditText tempEmail = (EditText) findViewById(R.id.loginID);
+        final Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edit.putString("emailTemp", tempEmail.getText().toString());
+                Log.i("on click", tempEmail.getText().toString());
+                edit.apply();
+
+                Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i("LoginActivity", "In onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i("LoginActivity", "In onStop()");
+        super.onStop();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.disconnect();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i("LoginActivity", "In onDestroy()");
+        super.onDestroy();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Login Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
 }
--- app/src/main/java/com/example/user/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/user/androidlabs/StartActivity.java
@@ -1,15 +1,77 @@
 package com.example.user.androidlabs;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("StartActivity", "In onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, ListItemsActivity.class);
+                startActivityForResult(intent,5);
+            }
+
+
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 5){
+            Log.i("ActivityResult","Returned to StartActivity.onActivity");
+        }
+        if (resultCode == Activity.RESULT_OK){
+            String messagePassed = "ListItemsAvtivity passed: "+data.getStringExtra("Response");
+            int duration = Toast.LENGTH_LONG;
 
+            Context context = getApplicationContext();
+            Toast toast = Toast.makeText(context,messagePassed,duration);
+            toast.show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i("StartActivity", "In onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i("StartActivity", "In onStart()");
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i("StartActivity", "In onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i("StartActivity", "In onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i("StartActivity", "In onDestroy()");
+        super.onDestroy();
+    }
 }
